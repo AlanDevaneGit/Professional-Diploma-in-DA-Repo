4d46c1fe048ca1d8f89509b657c4ab5055f96ac8
AlanDevaneGit <alandevane13@gmail.com>
2021-01-25 00:35:49 +0000

    Added FPL Dataset - Tested basic Pyhton Functions

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 #Setting up files for Project Rubric Assignment
+
+# Imports
+import pandas as pd
+
+# Importing CSV using Pandas Dataframe - Data from : https://www.kaggle.com/idoyo92/epl-stats-20192020
+data = pd.read_csv('players_1920_fin.csv')
+
+# Checking Data size( Print Col & Row's) using shape
+print(data.shape)
+
+# Prints out first 30  rows
+print(data.head(30))
+
+# Checking for missing data
+print(data.isnull().values.any())
+
+# Printing out FPL player names
+player_names = pd.unique(data.full)
+print(player_names)
+
+
